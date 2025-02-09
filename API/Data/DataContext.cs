b8c18d69e94d9daec1ced637ea130e869e8d1e8c
KwanTNg <kwantungng2@ln.hk>
2025-02-09 18:40:25 +0000

    First Commit

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,10 @@
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class DataContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<AppUser> Users { get; set; }
+    // Users is the name of our table in database
+}
